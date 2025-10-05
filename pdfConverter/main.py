010866d501193245f0750bad4ea34737b08f998d
Mehmet Değirmenci <119043096+degirmencimehmet@users.noreply.github.com>
2025-10-05 23:15:09 +0300

    Add files via upload

---
+++ pdfConverter/main.py
@@ -0,0 +1,16 @@
+# belirli bir pdfi docxe çevirme işlemi için :
+
+# from pdf2docx import Converter
+# yukarıdaki kısmı install etmeden çalışmaz onu install etmeyeceğim şuan
+
+
+# pdf_file = 'path kısmı'
+# docx_file = 'nereye kaydedilecek o kısım'
+
+# bir tane converter oluşturmamız lazım
+
+# cv = Converter(pdf_file)
+# cv.convert(docx_file)
+# cv.close()
+
+
